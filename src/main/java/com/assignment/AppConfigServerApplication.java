0abd1ad6b881d8e2949dd0b2335d0b6f14c8d6fa
Rajkumar ANGARE <rajangare@gmail.com>
2020-09-06 18:52:11 +0530

    Discovery client

--- src/main/java/com/assignment/AppConfigServerApplication.java
+++ src/main/java/com/assignment/AppConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class AppConfigServerApplication {
 
 	public static void main(String[] args) {
