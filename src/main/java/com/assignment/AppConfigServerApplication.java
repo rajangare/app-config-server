90df0c69ce59e195dc3d8aa0abe4854228658c9c
Rajkumar ANGARE <rajangare@gmail.com>
2020-09-06 19:41:25 +0530

    Discovery client

--- src/main/java/com/assignment/AppConfigServerApplication.java
+++ src/main/java/com/assignment/AppConfigServerApplication.java
@@ -2,12 +2,12 @@ package com.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class AppConfigServerApplication {
 
 	public static void main(String[] args) {
